9d29ffa12211dce007aeb47dc09820938d3955d8
jisu <ddc02336@gmail.com>
2025-07-07 19:04:19 +0900

    -visual slide text작업중..

---
+++ js/style.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    // autoplay: {
+    //   delay: 1900,
+    //   disableOnInteraction: false, // 사용자가 슬라이더를 클릭하거나 드래그했을 때 autoplay를 멈출 것인가?
+    // },
+  });
+};
