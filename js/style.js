b9243ee9e9acd8b5a8f9c518c8be58be556942a0
jisu <ddc02336@gmail.com>
2025-07-09 21:17:41 +0900

    -best_section swiper 수정해야함 -point_section hover하는중

--- js/style.js
+++ js/style.js
@@ -6,4 +6,22 @@ window.onload = function () {
     //   disableOnInteraction: false, // 사용자가 슬라이더를 클릭하거나 드래그했을 때 autoplay를 멈출 것인가?
     // },
   });
+  var swiper2 = new Swiper(".ustorySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 35,
+    pagination: {
+      el: ".custom-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return `<span class="${className}><img src="images/pagination/dot${index + 1}.png" alt="pagination" /></span>`;
+      },
+    },
+  });
+
+  // point_section hover시 이미지변경
+  const img = document.querySelector(".hover-change");
+
+  img.addEventListener("hover", () => {
+    img.src = "images/point_section/airism_hover_img.png";
+  });
 };
